c0731183930bfe2d9c377e7215f2e1eb7b3ac476
Brandon Phillips <brandon.phillips976@gmail.com>
2021-01-08 18:11:28 -0800

    created routes with full CRUD functionality for user model

---
+++ routes/user.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const db = require('../models')
+
+router.get('/user/all', (req, res) => {
+    db.User.find({}, (err, data) => {
+        res.json(data)
+    })
+})
+
+router.post('/user/create', (req, res) => {
+    console.log(req.body)
+    // first check if user with same email already exists
+    db.User.find({ email: req.body.email }, (err, data) => {
+        // if user exists, send status 409 to client
+        if (data.length > 0) {
+            res.status(409).end();
+        } else {
+            // if no user with same email exists, add user to db
+            db.User.create(req.body, (err, data) => {
+                if (err) console.log(err);
+                else {
+                    res.status(200).end();
+                }
+            })
+        }
+    })
+})
+
+router.put('/user/update/:id', (req, res) => {
+    db.User.updateOne({ _id: req.params.id }, { $set: req.body }, (err, data) => {
+        if (err) console.log(err)
+        else res.status(200).end();
+    })
+})
+
+router.delete('/user/delete/:id', (req, res) => {
+    // delete user from db by their id
+    db.User.deleteOne({ _id: req.params.id }, (err, data) => {
+        if (err) console.log(err);
+        else res.json(data).end();
+    })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -9,15 +9,15 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 
-// routes
-// const routes = require('./routes')
-// app.use(routes, '/api')
-
 // middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/blog-cms', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
+// routes
+const user = require('./routes/user')
+app.use('/api', user)
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/blog-cms', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
 
 app.listen(PORT, () => {
     console.log('server listening on port: ' + PORT)
